ec93500af972e617187d58bccb6af762bdfed262
Paulale1208 <pachecorpaula13@gmail.com>
2026-03-23 16:57:28 -0400

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+nombre_usuario = "paul"       #   una variable "nombe_usuario"   que guarda el texto "paul"
+edad = 17                     #   una variable "edad"    que guarda el numero 17
+hobbie = "futbol"             #   una variabe "hobbie"    que guarda el texto "futbol"
+
+print(nombre_usuario)         
+print(edad)                   
+print(hobbie)                 
+
+print(f"nombre de usuario"{paul} "edad" {17} "hobbie" {futbol}  )
---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+print ("calculador de area rectangulo ")
+
+base = int(input( "valor de base " ))
+altura= int(input( "valor de altura"))
+area = (base*altura) 
+
+print = (area)
